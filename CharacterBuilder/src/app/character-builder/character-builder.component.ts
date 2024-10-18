d593dfeef5e3bd83ee6e3f3848db025c7a5db0ce
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-10-18 11:55:38 +0200

    added building image

--- CharacterBuilder/src/app/character-builder/character-builder.component.ts
+++ CharacterBuilder/src/app/character-builder/character-builder.component.ts
@@ -1,24 +1,35 @@
 import { Component, inject, signal } from '@angular/core';
 import { WebApiService } from '../web-api.service';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-character-builder',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './character-builder.component.html',
   styleUrl: './character-builder.component.css'
 })
 export class CharacterBuilderComponent {
-  eye = signal<string>('');
-  mouth = signal<string>('');
-  hand = signal<string>('');
+  eye = signal<string>('No Eye');
+  mouth = signal<string>('No Mouth');
+  hand = signal<string>('No Hand');
   hasHammer = signal<boolean>(false);
   hasTail = signal<boolean>(false);
+  imageUrl = signal<string>('');
 
   private webApiService: WebApiService = inject(WebApiService);
 
-  buildImage() {
+  async buildImage() {
+    let url: string = await this.webApiService
+    .buildImage(this.eye().replace(" ", ""), this.mouth().replace(" ", ""), this.hand().replace(" ", ""), this.hasHammer(), this.hasTail());
+    this.imageUrl.set(url);
+  }
 
+  reset() {
+    this.eye.set('');
+    this.mouth.set('');
+    this.hand.set('');
+    this.hasHammer.set(false);
+    this.hasTail.set(false);
   }
 }
--- CharacterBuilder/src/app/web-api.service.ts
+++ CharacterBuilder/src/app/web-api.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+interface imageResponse {
+  url: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebApiService {
+  readonly apiUrl: string = 'http://localhost:5110/';
+  private httpClient = inject(HttpClient);
 
-  constructor() { }
+  async buildImage(eye: string, mouth: string, hand: string, hasHammer: boolean, hasTail: boolean): Promise<string> {
+    console.log(eye, mouth, hand, hasHammer, hasTail);
+    const response = await lastValueFrom(this.httpClient.post<imageResponse>(this.apiUrl + "build-image-url", {
+      eye: eye,
+      hasHammer: hasHammer,
+      mouth: mouth,
+      rightHand: hand,
+      hasTail: hasTail
+    }));
+    console.log(response);
+    return response.url;
+  }
 }
